89895a4ce68428c79a5d39596f2dc66201e87a91
Paulina Ester Egvarth Rehbein <68613088+PaulinaEster@users.noreply.github.com>
2022-05-11 19:54:51 -0300

    Add files via upload
    arquivo atividade prolog aula 05
    
    
    
    ?- consult(paulinaAula05).
    true.
    
    ?- ministra(X, Y).
    X = joao,
    Y = paradigmas ;
    X = joao,
    Y = gerencia ;
    X = joao,
    Y = algoritmos ;
    X = paula,
    Y = programacao.
    
    ?- ministra(jaoa, programacao).
    false.
    
    ?- ministra(joao, paradigmas).
    true.
    
    ?- atua_curso(X,Y).
    X = joao,
    Y = sistemas ;
    X = joao,
    Y = licenciatura 
    Unknown action: / (h for help)
    Action? 
    Unknown action: (h for help)
    Action? 
    Unknown action:  (h for help)
    Action? ;
    X = paula,
    Y = sistemas.
    
    ?- atua_curso(paula, sistemas).
    true.
    
    ?- atua_curso(joao, paradigmas).
    false.
    
    ?-

---
+++ paulinaAula05.pl
@@ -0,0 +1,13 @@
+atua_curso(joao, sistemas).
+atua_curso(joao, licenciatura).
+atua_curso(paula, sistemas).
+
+ministra(joao, paradigmas).
+ministra(joao, gerencia).
+ministra(joao, algoritmos).
+ministra(paula, programacao).
+
+
+
+
+
